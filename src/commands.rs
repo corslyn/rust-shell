b023464055ef5dd85d36985ade50a20c9e1d2f32
Enzo Manzinali <enzomanzinali@protonmail.com>
2024-12-16 22:01:19 +0100

    Add commands mod

---
+++ src/commands.rs
@@ -0,0 +1,13 @@
+pub struct ShellCommands;
+
+pub enum Command {
+    // TODO : Add commands
+}
+
+impl Command {
+
+}
+
+impl ShellCommands {
+    // add commands here
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod commands;
 fn main() {
     println!("Hello, world!");
 }
