1d96ea776f0d19d0f4fa3bd8e6a9b8187e0f56c5
Enzo Manzinali <enzomanzinali@protonmail.com>
2024-12-16 22:21:47 +0100

    Add exit command

--- src/commands.rs
+++ src/commands.rs
@@ -2,12 +2,26 @@ pub struct ShellCommands;
 
 pub enum Command {
     // TODO : Add commands
+    Exit,
+    Unknown(String),
 }
 
 impl Command {
-
+    pub fn from_str(command: &str) -> Command {
+        match command {
+            "exit" => Command::Exit,
+            _ => Command::Unknown(command.to_string()),
+        }
+    }
 }
 
 impl ShellCommands {
     // add commands here
+    pub fn exit(exit_code: i32) {
+        std::process::exit(exit_code)
+    }
+
+    pub fn unknown(command: &str) {
+        eprintln!("{}: command not found", command);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
+use std::{io::{self, Read, Write}, process::exit};
+
 mod commands;
+use commands::{Command, ShellCommands};
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        let trimmed_input = input.trim();
+        if trimmed_input.is_empty() {
+            continue;
+        }
+
+        let parts: Vec<&str> = trimmed_input.split_whitespace().collect();
+        let command_str = parts[0];
+        let args = &parts[1..];
+
+        match Command::from_str(command_str) {
+            Command::Exit => {
+                // Check if an argument is provided for the exit code
+                if let Some(arg) = args.get(0) {
+                    match arg.parse::<i32>() {
+                        Ok(exit_code) => ShellCommands::exit(exit_code),
+                        Err(_) => {
+                            ShellCommands::exit(1);
+                            continue;
+                        }
+                    }
+                } else {
+                    ShellCommands::exit(0); // Default to exit code 0
+                }
+            }
+            Command::Unknown(_) => ShellCommands::unknown(command_str),
+        }
+    }
 }
